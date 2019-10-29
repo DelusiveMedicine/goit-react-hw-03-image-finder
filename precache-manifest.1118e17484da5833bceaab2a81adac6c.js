2def8e77c763e15f6558b141a741debaa7b3d04b
Stacy <enjoyorhate@gmail.com>
2019-10-29 23:46:32 +0200

    Updates

---
+++ precache-manifest.1118e17484da5833bceaab2a81adac6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "057c46c0b52cbc91640ac148f157ad1b",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "ed6fca5a6feadf1c62d2",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.baf32977.chunk.css"
+  },
+  {
+    "revision": "cbd9e860bfd65f50a7fd",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.2311d43c.chunk.js"
+  },
+  {
+    "revision": "ed6fca5a6feadf1c62d2",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.6ab54eac.chunk.js"
+  },
+  {
+    "revision": "7e3234e7aefa8359527f",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.3cf19af0.js"
+  }
+]);
